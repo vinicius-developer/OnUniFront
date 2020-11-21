92f7163f0414c483b36bd62895143b59c9ea6759
Vinicius de Paula <viniciusilvav1@hotmail.com>
2020-11-21 16:07:54 -0300

    cadastro de ongs

--- cadastro/js/cadOng.js
+++ cadastro/js/cadOng.js
@@ -1,33 +1,5 @@
-// document.getElementById('cadastrar').addEventListener('click', (e) => {
-//     e.preventDefault()
-
-//     //     options['method'] = 'POST';
-//     options['mode'] = 'cors';
-//     options['header'] = {
-//         'Content-Type': 'application/json',
-//         'Access-Control-Allow-Methods': "GET, PUT, DELETE, POST",
-//         'Access-Control-Allow-Heders': "rigin, X-Requested-With, Content-Type, Accept, Authorization*",
-//         'Access-Control-Allow-Origin': "*",
-//         "Status": "200 OK",
-//         "Vary": "Accept"
-//     }
-//     options['body'] = JSON.stringify(data)
-
-
-//     async function sendRegisterOng(options) {
-//         const response = await fetch("http://127.0.0.1:8000/api/ong/auth/register", options)
-//         return response
-//     }
-
-//     sendRegisterOng(options)
-//         .then(response => console.log(response))
-//         .catch(error => console.log(error.message))
-// })
-
-
 document.getElementById('cadastrar').addEventListener('click', (e) => {
     e.preventDefault();
-
 
     const elementsForm = document.getElementById('form').elements
     const telefones = document.getElementsByClassName('telefones');
@@ -35,7 +7,6 @@ document.getElementById('cadastrar').addEventListener('click', (e) => {
     const header = {
         'Content-Type': 'application/json'
     }
-
     data['telefones'] = []
 
     for(let i = 0; i < elementsForm.length - 1; i++) {
@@ -46,14 +17,33 @@ document.getElementById('cadastrar').addEventListener('click', (e) => {
         data.telefones[i] = telefones[i].value
     }
 
-    async function testeTeste(body, header) {
-
-        const response = await (axios.post('http://127.0.0.1:8000/api/ong/auth/register', body, header)).data
+    async function send(body, header) {
+        const response = await axios.post('http://127.0.0.1:8000/api/ong/auth/register', body, header)
+            .then(response => response.data.message)
+            .catch(error => {
+                const errors = error.response.data.errors
+                alertErrors(errors)
+            })
 
         return response
-
     }
 
-    console.log(testeTeste(data, header))
+    send(data, header)
 
+    function alertErrors(error) {
+        const campos = Object.keys(error)
+        const messages = Object.values(error)
+
+        console.log(messages)
+
+        for(let i = 0; i < campos.length; i++){
+            const elementos = document.getElementById(campos[i]).nextElementSibling
+            let nameElement = document.getElementById(campos[i]).previousElementSibling.textContent.slice(0, -3)
+
+            elementos.innerHTML = 'O campo ' + nameElement + ' é '+ messages[i]
+
+        }
+
+
+    }
 })
