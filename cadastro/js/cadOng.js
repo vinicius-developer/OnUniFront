fc705fd20e545b2fef693f3092b24cbeccea1287
Vinicius de Paula <viniciusilvav1@hotmail.com>
2020-11-22 22:23:04 -0300

    tela escolha, registro ong

--- cadastro/js/cadOng.js
+++ cadastro/js/cadOng.js
@@ -9,12 +9,12 @@ document.getElementById('cadastrar').addEventListener('click', (e) => {
     }
     data['telefones'] = []
 
-    for(let i = 0; i < elementsForm.length - 1; i++) {
-         if(elementsForm[i].id !== '') data[elementsForm[i].id] = elementsForm[i].value
+    for (let i = 0; i < elementsForm.length - 1; i++) {
+        if (elementsForm[i].id !== '') data[elementsForm[i].id] = elementsForm[i].value
     }
 
-    for(let i = 0; i < telefones.length; i++) {
-        data.telefones[i] = telefones[i].value
+    for (let i = 0; i < telefones.length; i++) {
+        if(telefones[i].value)  data.telefones[i] = telefones[i].value
     }
 
     async function send(body, header) {
@@ -22,9 +22,9 @@ document.getElementById('cadastrar').addEventListener('click', (e) => {
             .then(response => response.data.message)
             .catch(error => {
                 const errors = error.response.data.errors
+                clear(errors)
                 alertErrors(errors)
             })
-
         return response
     }
 
@@ -34,16 +34,37 @@ document.getElementById('cadastrar').addEventListener('click', (e) => {
         const campos = Object.keys(error)
         const messages = Object.values(error)
 
-        console.log(messages)
-
-        for(let i = 0; i < campos.length; i++){
-            const elementos = document.getElementById(campos[i]).nextElementSibling
-            let nameElement = document.getElementById(campos[i]).previousElementSibling.textContent.slice(0, -3)
-
-            elementos.innerHTML = 'O campo ' + nameElement + ' é '+ messages[i]
-
+        for (let i = 0; i < campos.length; i++) {
+            if(campos[i] !== 'telefones') {
+                const elementos = document.getElementById(campos[i]).nextElementSibling
+                elementos.innerHTML = `<p>${messages[i]}<p>`
+            } else {
+                const elementos = document.getElementsByClassName(campos[i])[0].nextElementSibling
+                elementos.innerHTML = `<p>${messages[i]}</p>`
+            }
         }
-
-
     }
+
+    function clear(error) {
+        const campos = Object.keys(error)
+        const prefix = ['cnpj', 'email', 'nome_fantasia', 'razao_social', 'password', 'rua', 'cep', 'numero', 'bairro', 'cidade', 'descricao']
+        for (let i = 0; i < campos.length; i++) {
+            const campo = prefix.indexOf(campos[i])
+            prefix.splice(campo, 1)
+        }
+        for (let i = 0; i < prefix.length; i++) {
+            const element = document.getElementById(prefix[i]).nextElementSibling.firstChild
+            if(element != null) {
+                console.log(element)
+                element.remove()
+            }
+        }
+    }
+
+    function success() {
+        console.log('A')
+    }
+
+
+
 })
